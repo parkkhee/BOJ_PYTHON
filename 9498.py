91ea628e65e3a38d777da03824bbfbc8b777a2a9
parkkhee <76894305+parkkhee@users.noreply.github.com>
2022-07-11 12:53:37 +0900

    Create 9498.py

---
+++ 9498.py
@@ -0,0 +1,13 @@
+x= int(input())
+
+if(100>=x>=90):
+    print('A')
+elif(89>=x>=80):
+    print('B')
+elif(79>=x>=70):
+    print('C')
+elif(69>=x>=60):
+    print('D')
+else:
+    print('F')
+    
